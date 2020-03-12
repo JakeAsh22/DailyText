91db46a27d9b10ac895878da6acb6c3d22d814cd
JakeAsh22 <jacob.ash1998@gmail.com>
2020-03-12 10:09:21 -0700

    Add a field to send whatever message you want to the bot

--- src/components/GroupMe.jsx
+++ src/components/GroupMe.jsx
@@ -5,20 +5,29 @@ import PropTypes from 'prop-types'
 
 const API_KEY = process.env.API_KEY;
 const BOT_ID = 'de88fdd446241f720bb0ff1834';
-const TEXT = 'Oh my gosh Wei Dai is so cool'
+
 console.log(BOT_ID)
 class GroupMe extends Component {
-  
+
   constructor(props) {
     super(props)
-    this.state = {data: []}
-    
+    this.state = {
+      data: [],
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+
   }
-  
-  postMsg()  {
-    
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+
+  postMsg() {
+    console.log(this.state.value)
+    let TEXT = this.state.value
     fetch(`https://api.groupme.com/v3/bots/post?bot_id=${BOT_ID}&text=${TEXT}`, {
-      method: 'POST', 
+      method: 'POST',
     })
       .then((response) => response.json())
       .then((data) => {
@@ -30,8 +39,15 @@ class GroupMe extends Component {
   }
   render() {
     return (
-      
+
       <div>
+        <form>
+          <label>
+            Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <h1>Add buttons</h1>
         <button onClick={() => this.postMsg()}>Send Message</button>
       </div>
